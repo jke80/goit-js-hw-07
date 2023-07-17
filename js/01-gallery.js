2780dea4becf419a7cc0b73ff2c15ff550362b1d
Evhenii <kolomoycev81@gmail.com>
2023-07-18 00:02:54 +0300

    gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) => `<li class ="gallery__item">
+    <a href="${original}" class="gallery__link">
+    <img class="gallery__image" src="${preview}" 
+    data-source="${original}"
+     alt="${description}">
+     </a>
+     </li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) return;
+
+  const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+  document.addEventListener("keydown", onKeyDown);
+
+  function onKeyDown(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onKeyDown);
+    }
+  }
+}
